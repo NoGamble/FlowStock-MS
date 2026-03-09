7284440bd036ef3accfacc034b9ea8050bb048c4
NoGamble <uestc520@gmail.com>
2026-03-09 23:27:31 +0800

    feat: re-initialize project with clean structure

---
+++ src/main/java/com/flowstock/ms/FlowStockMsApplication.java
@@ -0,0 +1,13 @@
+package com.flowstock.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowStockMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowStockMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flowstock/ms/FlowStockMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowstock.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowStockMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
