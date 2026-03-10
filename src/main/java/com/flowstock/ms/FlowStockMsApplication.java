820181b825632f9deae9eb63982bd0d04f59b070
NoGamble <uestc520@gmail.com>
2026-03-10 15:24:24 +0800

    demo

--- src/main/java/com/flowstock/ms/FlowStockMsApplication.java
+++ src/main/java/com/flowstock/ms/FlowStockMsApplication.java
@@ -1,7 +1,9 @@
 package com.flowstock.ms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlowStockMsApplication {
@@ -10,4 +12,14 @@ public class FlowStockMsApplication {
         SpringApplication.run(FlowStockMsApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner testJPA(InventoryRepository repo) {
+        return args -> {
+            System.out.println("---- 开始读取数据库数据 ----");
+            repo.findAll().forEach(item -> {
+                System.out.println("商品名称: " + item.getItemName() + " | 数量: " + item.getQuantity());
+            });
+            System.out.println("---- 读取结束 ----");
+        };
+    }
 }
